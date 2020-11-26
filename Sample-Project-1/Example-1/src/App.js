a6a4f7007f5a878a42024936feba105018b24048
ChristoT98 <christolakshan@gmail.com>
2020-11-26 12:01:45 +0530

    Created Example-1

---
+++ Sample-Project-1/Example-1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./Header";
+import AppLink from "./AppLink";
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <AppLink />
+    </div>
+  );
+}
+
+export default App;
